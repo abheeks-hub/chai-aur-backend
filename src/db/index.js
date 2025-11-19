472b73bbf215880636d5e3fbba8da811eaabac93
Abheek Samaddar <abheek.samaddar33pmi@gmail.com>
2025-11-19 22:51:42 +0530

    .

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants"
+import { DB_NAME } from "../constants.js";
 
-const connectDB = async ()=>{
-    try {
-        
-    } catch (error) {
-        console.log("MONGO DB CONNECTION ERROR: ", error);
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected✅, DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Mongo DB connection error!", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+// require('dotenv').config({path: './env'})
+
 import dotenv from "dotenv";
-dotenv.config();
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+dotenv.config({path: './env'});
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
 
 // SECOND APPROACH 
+
+
+
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
 
 
 
